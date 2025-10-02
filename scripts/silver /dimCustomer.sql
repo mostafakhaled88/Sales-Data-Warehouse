dc546a29bda3ff438d3d9901f504aead81165a0a
mostafakhaled88 <mosta.mk@gmail.com>
2025-10-02 10:01:29 +0300

    Update dimCustomer.sql

--- scripts/silver /dimCustomer.sql
+++ scripts/silver /dimCustomer.sql
@@ -1,3 +1,33 @@
+-- 1. Drop referencing constraints first
+DECLARE @sql NVARCHAR(MAX) = N'';
+
+SELECT @sql += 'ALTER TABLE ' + QUOTENAME(OBJECT_SCHEMA_NAME(parent_object_id)) 
+             + '.' + QUOTENAME(OBJECT_NAME(parent_object_id)) 
+             + ' DROP CONSTRAINT ' + QUOTENAME(name) + ';' + CHAR(13)
+FROM sys.foreign_keys
+WHERE referenced_object_id = OBJECT_ID('SalesDWH.silver.DimCustomer');
+
+EXEC sp_executesql @sql;
+
+-- 2. Drop the table
+IF OBJECT_ID('SalesDWH.silver.DimCustomer', 'U') IS NOT NULL
+    DROP TABLE SalesDWH.silver.DimCustomer;
+GO
+
+-- 3. Recreate table
+CREATE TABLE [SalesDWH].[silver].[DimCustomer] (
+    CustomerKey INT IDENTITY(1,1) PRIMARY KEY,
+    CustomerName NVARCHAR(200) NOT NULL,
+    Phone NVARCHAR(50) NULL,
+    AddressLine NVARCHAR(250) NULL,
+    LocationKey INT NOT NULL,
+    CONSTRAINT FK_DimCustomer_DimLocation FOREIGN KEY (LocationKey)
+        REFERENCES [SalesDWH].[silver].[DimLocation](LocationKey)
+);
+
+
+
+
 INSERT INTO [SalesDWH].[silver].[dimCustomer] 
     (CustomerName, Phone, AddressLine, LocationKey)
 SELECT DISTINCT
