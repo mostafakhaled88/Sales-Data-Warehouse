739c6b5e2983f137002ce25713c1b898d22b242b
mostafakhaled88 <mosta.mk@gmail.com>
2025-10-04 09:19:27 +0300

    Update ddl_bronze.sql

--- scripts/bronze /ddl_bronze.sql
+++ scripts/bronze /ddl_bronze.sql
@@ -8,7 +8,6 @@ Script Purpose:
 	  Run this script to re-define the DDL structure of 'bronze' Tables
 ===============================================================================
 */
-
 USE SalesDWH;
 GO
 
@@ -20,36 +19,29 @@ GO
 -- Create Bronze Raw Table (all as NVARCHAR)
 CREATE TABLE bronze.sales_raw
 (
-    -- Customer Info
-    CustomerId          NVARCHAR(50) NULL,
-    FirstName           NVARCHAR(100) NULL,
-    LastName            NVARCHAR(100) NULL,
-    City                NVARCHAR(100) NULL,
-    Country             NVARCHAR(100) NULL,
-    Phone               NVARCHAR(50) NULL,
-
-    -- Order Info
-    OrderId             NVARCHAR(50) NULL,
-    OrderDate           NVARCHAR(50) NULL,
-    OrderNumber         NVARCHAR(50) NULL,
-    TotalAmount         NVARCHAR(50) NULL,
-
-    -- Order Item Info
-    OrderItemId         NVARCHAR(50) NULL,
-    ProductId           NVARCHAR(50) NULL,
-    Order_UnitPrice     NVARCHAR(50) NULL,
-    Quantity            NVARCHAR(50) NULL,
-
-    -- Product Info
-    ProductName         NVARCHAR(200) NULL,
-    SupplierId          NVARCHAR(50) NULL,
-    Product_UnitPrice   NVARCHAR(50) NULL,
-    Package             NVARCHAR(100) NULL,
-    IsDiscontinued      NVARCHAR(50) NULL,
-
-    -- Supplier Info
-    SupplierName        NVARCHAR(200) NULL
+    ORDERNUMBER INT,
+    QUANTITYORDERED INT,
+    PRICEEACH DECIMAL(10,2),
+    ORDERLINENUMBER INT,
+    SALES DECIMAL(12,2),
+    ORDERDATE DATETIME,
+    STATUS NVARCHAR(50),
+    QTR_ID INT,
+    MONTH_ID INT,
+    YEAR_ID INT,
+    PRODUCTLINE NVARCHAR(100),
+    MSRP DECIMAL(10,2),
+    PRODUCTCODE NVARCHAR(50),
+    CUSTOMERNAME NVARCHAR(255),
+    PHONE NVARCHAR(50),
+    ADDRESSLINE1 NVARCHAR(255),
+    ADDRESSLINE2 NVARCHAR(255),
+    CITY NVARCHAR(100),
+    STATE NVARCHAR(100),
+    POSTALCODE NVARCHAR(20),
+    COUNTRY NVARCHAR(100),
+    TERRITORY NVARCHAR(50),
+    CONTACTLASTNAME NVARCHAR(100),
+    CONTACTFIRSTNAME NVARCHAR(100),
+    DEALSIZE NVARCHAR(50)
 );
-GO
-
-PRINT 'Bronze layer table [bronze.sales_raw] created successfully.';
