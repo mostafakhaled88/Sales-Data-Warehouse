17474d3d1779cafd9e0b89e6f5ac09095c98a595
mostafakhaled88 <mosta.mk@gmail.com>
2025-10-04 08:12:28 +0300

    Update proc_load_bronze.sql

--- scripts/bronze /proc_load_bronze.sql
+++ scripts/bronze /proc_load_bronze.sql
@@ -1,43 +1,69 @@
 /*
-=============================================================
-Procedure: bronze.proc_load_bronze
-=============================================================
-Purpose:
-    - Load raw CSV data into Bronze layer using BULK INSERT.
-    - Path: C:\SQLData\sales_data.csv
-    - This procedure truncates the raw table before loading.
-=============================================================
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
 */
 
 USE SalesDWH;
 GO
 
-IF OBJECT_ID('bronze.proc_load_bronze', 'P') IS NOT NULL
-    DROP PROCEDURE bronze.proc_load_bronze;
+IF OBJECT_ID('bronze.load_bronze', 'P') IS NOT NULL
+    DROP PROCEDURE bronze.load_bronze;
 GO
 
-CREATE PROCEDURE bronze.proc_load_bronze
-AS
-BEGIN
-    SET NOCOUNT ON;
+CREATE PROCEDURE bronze.load_bronze as
 
-    PRINT '🔄 Starting Bronze Load...';
+	BEGIN
+		DECLARE @batch_start_time DATETIME, @batch_end_time DATETIME; 
+		BEGIN TRY
+			SET @batch_start_time = GETDATE();
+			PRINT '================================================';
+			PRINT 'Loading Bronze Layer';
+			PRINT '================================================';
 
-    -- Truncate existing data
-    TRUNCATE TABLE bronze.sales_raw;
+		
 
-    -- Bulk insert from CSV
-    BULK INSERT bronze.sales_raw
-    FROM 'C:\SQLData\sales_data.csv'
-    WITH 
-    (
-        FIRSTROW = 2,                 -- Skip header row
-        FIELDTERMINATOR = ',',        -- Column delimiter
-        ROWTERMINATOR = '0x0a',       -- Line feed (\n)
-        TABLOCK,
-        CODEPAGE = '65001'            -- UTF-8 support
-    );
+		-- Truncate existing data
+		TRUNCATE TABLE bronze.sales_raw;
 
-    PRINT '✅ Bronze Load Completed Successfully.';
-END;
-GO
+		-- Bulk insert from CSV
+		BULK INSERT bronze.sales_raw
+		FROM 'C:\SQLData\sales_data.csv'
+		WITH 
+		(
+			FIRSTROW = 2,                 -- Skip header row
+			FIELDTERMINATOR = ',',        -- Column delimiter
+			ROWTERMINATOR = '0x0a',       -- Line feed (\n)
+			TABLOCK,
+			CODEPAGE = '65001'            -- UTF-8 support
+		);
+
+		PRINT ' Bronze Load Completed Successfully.';
+	SET @batch_end_time = GETDATE();
+			PRINT '=========================================='
+			PRINT 'Loading Bronze Layer is Completed';
+			PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+			PRINT '=========================================='
+		END TRY
+		BEGIN CATCH
+			PRINT '=========================================='
+			PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+			PRINT 'Error Message' + ERROR_MESSAGE();
+			PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+			PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+			PRINT '=========================================='
+		END CATCH
+	END
