ed34921b0a5a1798716c811866fe3cd39984d862
mostafakhaled88 <mosta.mk@gmail.com>
2025-10-05 09:02:11 +0300

    Update silver.load_silve.sql

--- scripts/silver /silver.load_silve.sql
+++ scripts/silver /silver.load_silve.sql
@@ -33,15 +33,75 @@ BEGIN
 
     BEGIN TRY
         -------------------------------------------------------------------
-        -- 1. Clear existing data
+        -- 1. Drop Foreign Key Constraints
+        -------------------------------------------------------------------
+        PRINT '→ Dropping Foreign Key Constraints...';
+        IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'FK_FactSales_DimCustomer')
+            ALTER TABLE silver.FactSales DROP CONSTRAINT FK_FactSales_DimCustomer;
+
+        IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'FK_FactSales_DimProduct')
+            ALTER TABLE silver.FactSales DROP CONSTRAINT FK_FactSales_DimProduct;
+
+        IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'FK_FactSales_DimDate')
+            ALTER TABLE silver.FactSales DROP CONSTRAINT FK_FactSales_DimDate;
+
+        -------------------------------------------------------------------
+        -- 2. Truncate Tables
         -------------------------------------------------------------------
         PRINT '→ Truncating Silver Tables...';
         TRUNCATE TABLE silver.FactSales;
         TRUNCATE TABLE silver.DimCustomer;
         TRUNCATE TABLE silver.DimProduct;
+        TRUNCATE TABLE silver.DimDate;
 
         -------------------------------------------------------------------
-        -- 2. Load DimCustomer
+        -- 3. Load DimDate
+        -------------------------------------------------------------------
+        PRINT '→ Loading DimDate...';
+
+        DECLARE @MinDate DATE, @MaxDate DATE;
+
+        SELECT 
+            @MinDate = MIN(TRY_CAST(OrderDate AS DATE)),
+            @MaxDate = MAX(TRY_CAST(OrderDate AS DATE))
+        FROM bronze.sales_raw;
+
+        ;WITH AllDates AS (
+            SELECT @MinDate AS FullDate
+            UNION ALL
+            SELECT DATEADD(DAY, 1, FullDate)
+            FROM AllDates
+            WHERE FullDate < @MaxDate
+        )
+        INSERT INTO silver.DimDate
+        (
+            FullDate,
+            [Day],
+            Month_ID,
+            MonthName,
+            Qtr_ID,
+            QuarterName,
+            Year_ID,
+            YearName,
+            dwh_create_date
+        )
+        SELECT 
+            FullDate,
+            DAY(FullDate),
+            MONTH(FullDate),
+            DATENAME(MONTH, FullDate),
+            DATEPART(QUARTER, FullDate),
+            'Q' + CAST(DATEPART(QUARTER, FullDate) AS NVARCHAR),
+            YEAR(FullDate),
+            CAST(YEAR(FullDate) AS NVARCHAR),
+            GETDATE()
+        FROM AllDates
+        OPTION (MAXRECURSION 0);
+
+        PRINT '✅ DimDate Load Completed.';
+
+        -------------------------------------------------------------------
+        -- 4. Load DimCustomer
         -------------------------------------------------------------------
         PRINT '→ Loading DimCustomer...';
         INSERT INTO silver.DimCustomer
@@ -51,34 +111,61 @@ BEGIN
             ContactLastName,
             Phone,
             AddressLine1,
-            AddressLine2,
             City,
             State,
             PostalCode,
             Country,
-            Territory,
-            DealSize
+            Territory
         )
         SELECT DISTINCT
-            LTRIM(RTRIM(CUSTOMERNAME)),
-            LTRIM(RTRIM(CONTACTFIRSTNAME)),
-            LTRIM(RTRIM(CONTACTLASTNAME)),
-            LTRIM(RTRIM(PHONE)),
-            LTRIM(RTRIM(ADDRESSLINE1)),
-            LTRIM(RTRIM(ADDRESSLINE2)),
-            LTRIM(RTRIM(CITY)),
-            LTRIM(RTRIM(STATE)),
-            LTRIM(RTRIM(POSTALCODE)),
-            LTRIM(RTRIM(COUNTRY)),
-            LTRIM(RTRIM(TERRITORY)),
-            LTRIM(RTRIM(DEALSIZE))
-        FROM bronze.sales_raw
-        WHERE CUSTOMERNAME IS NOT NULL;
+            REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE( LTRIM(RTRIM(b.CUSTOMERNAME)), '+', ''), '(', ''), ')', ''), '"', ''), '.', ''), ',', ''), '/', '') AS CustomerName,
+            REPLACE(LTRIM(RTRIM(b.CONTACTFIRSTNAME)),'-',''),
+            REPLACE(LTRIM(RTRIM(b.CONTACTLASTNAME)),'-',''),
+			dbo.KeepDigits(PHONE) AS Phone,  
+           LTRIM(RTRIM(
+                CASE 
+                    WHEN RIGHT(LTRIM(RTRIM(ADDRESSLINE1)), 1) IN ('.', ',') 
+                    THEN LEFT(LTRIM(RTRIM(ADDRESSLINE1)), LEN(LTRIM(RTRIM(ADDRESSLINE1))) - 1)
+                    ELSE LTRIM(RTRIM(ADDRESSLINE1))
+                END
+            )) AS AddressLine1,
+           LTRIM(RTRIM(
+    CASE 
+        WHEN RIGHT(LTRIM(RTRIM(CITY)), 1) IN ('.', ',') 
+        THEN LEFT(LTRIM(RTRIM(CITY)), LEN(LTRIM(RTRIM(CITY))) - 1)
+        ELSE LTRIM(RTRIM(CITY))
+    END
+)) AS City,
+           -- Clean State
+CASE 
+    WHEN STATE IS NULL THEN 'UNKNOWN'
+    ELSE UPPER(LTRIM(RTRIM(
+        CASE 
+            WHEN RIGHT(LTRIM(RTRIM(STATE)), 1) IN ('.', ',') 
+            THEN LEFT(LTRIM(RTRIM(STATE)), LEN(LTRIM(RTRIM(STATE))) - 1)
+            ELSE LTRIM(RTRIM(STATE))
+        END
+    )))
+END AS State,
+
+            CASE 
+        WHEN b.POSTALCODE IS NULL THEN 'Unknown'
+        ELSE UPPER(LTRIM(RTRIM(b.POSTALCODE)))
+    END AS PostalCode,
+            LTRIM(RTRIM(b.COUNTRY)),
+           CASE 
+    WHEN UPPER(LTRIM(RTRIM(b.COUNTRY))) IN ('USA','CANADA','MEXICO') THEN 'NA'
+    WHEN UPPER(LTRIM(RTRIM(b.COUNTRY))) IN ('FRANCE','GERMANY','ITALY','SPAIN','UK','BELGIUM','NORWAY','SWEDEN','AUSTRIA','SWITZERLAND','IRELAND','DENMARK','FINLAND') THEN 'EMEA'
+    WHEN UPPER(LTRIM(RTRIM(b.COUNTRY))) IN ('AUSTRALIA','SINGAPORE','JAPAN','PHILIPPINES') THEN 'APAC'
+    ELSE 'OTHER'
+END AS Territory
+        FROM bronze.sales_raw AS b
+        WHERE b.CUSTOMERNAME IS NOT NULL;
 
         PRINT '✅ DimCustomer Load Completed.';
 
         -------------------------------------------------------------------
-        -- 3. Load DimProduct
+        -- 5. Load DimProduct
         -------------------------------------------------------------------
         PRINT '→ Loading DimProduct...';
         INSERT INTO silver.DimProduct
@@ -88,16 +175,16 @@ BEGIN
             MSRP
         )
         SELECT DISTINCT
-            LTRIM(RTRIM(PRODUCTCODE)),
-            LTRIM(RTRIM(PRODUCTLINE)),
-            MSRP
-        FROM bronze.sales_raw
-        WHERE PRODUCTCODE IS NOT NULL;
+            LTRIM(RTRIM(b.PRODUCTCODE)),
+            LTRIM(RTRIM(b.PRODUCTLINE)),
+            b.MSRP
+        FROM bronze.sales_raw AS b
+        WHERE b.PRODUCTCODE IS NOT NULL;
 
         PRINT '✅ DimProduct Load Completed.';
 
         -------------------------------------------------------------------
-        -- 4. Load FactSales
+        -- 6. Load FactSales
         -------------------------------------------------------------------
         PRINT '→ Loading FactSales...';
         INSERT INTO silver.FactSales
@@ -108,29 +195,70 @@ BEGIN
             QuantityOrdered,
             PriceEach,
             Sales,
+            DealSize,
             Qtr_ID,
             Month_ID,
             Year_ID,
-            ProductCode
+            CustomerID,
+            ProductID,
+            dwh_create_date
         )
         SELECT 
-            ORDERNUMBER,
-            TRY_CAST(ORDERDATE AS DATETIME),
-            LTRIM(RTRIM(STATUS)),
-            QUANTITYORDERED,
-            PRICEEACH,
-            SALES,
-            QTR_ID,
-            MONTH_ID,
-            YEAR_ID,
-            PRODUCTCODE
-        FROM bronze.sales_raw
-        WHERE ORDERNUMBER IS NOT NULL;
+            b.ORDERNUMBER,
+            TRY_CAST(b.ORDERDATE AS DATETIME),
+            LTRIM(RTRIM(b.STATUS)),
+            b.QUANTITYORDERED,
+            b.PRICEEACH,
+            b.SALES,
+            LTRIM(RTRIM(b.DEALSIZE)),
+            b.QTR_ID,
+            b.MONTH_ID,
+            b.YEAR_ID,
+            c.CustomerID,
+            p.ProductID,
+            GETDATE()
+        FROM bronze.sales_raw AS b
+        LEFT JOIN silver.DimCustomer AS c
+            ON  REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE( LTRIM(RTRIM(b.CUSTOMERNAME)), '+', ''), '(', ''), ')', ''), '"', ''), '.', ''), ',', ''), '/', '')  = c.CustomerName
+        LEFT JOIN silver.DimProduct AS p
+            ON LTRIM(RTRIM(b.PRODUCTCODE)) = p.ProductCode;
 
         PRINT '✅ FactSales Load Completed.';
 
         -------------------------------------------------------------------
-        -- 5. Summary
+        -- 7. Map DateID in FactSales
+        -------------------------------------------------------------------
+        PRINT '→ Linking FactSales with DimDate...';
+        UPDATE f
+        SET f.DateID = d.DateID
+        FROM silver.FactSales AS f
+        INNER JOIN silver.DimDate AS d
+            ON CAST(f.OrderDate AS DATE) = d.FullDate;
+
+        -- Verify all DateIDs are populated
+        IF EXISTS (SELECT 1 FROM silver.FactSales WHERE DateID IS NULL)
+            THROW 50000, 'Some FactSales rows still have NULL DateID!', 1;
+
+        PRINT '✅ DateID Mapping Completed.';
+
+        -------------------------------------------------------------------
+        -- 8. Recreate Foreign Key Constraints
+        -------------------------------------------------------------------
+        PRINT '→ Recreating Foreign Key Constraints...';
+        ALTER TABLE silver.FactSales
+            ADD CONSTRAINT FK_FactSales_DimCustomer FOREIGN KEY (CustomerID)
+            REFERENCES silver.DimCustomer(CustomerID);
+
+        ALTER TABLE silver.FactSales
+            ADD CONSTRAINT FK_FactSales_DimProduct FOREIGN KEY (ProductID)
+            REFERENCES silver.DimProduct(ProductID);
+
+        ALTER TABLE silver.FactSales
+            ADD CONSTRAINT FK_FactSales_DimDate FOREIGN KEY (DateID)
+            REFERENCES silver.DimDate(DateID);
+
+        -------------------------------------------------------------------
+        -- 9. Summary
         -------------------------------------------------------------------
         DECLARE @end_time DATETIME = GETDATE();
         PRINT '================================================';
@@ -149,3 +277,4 @@ BEGIN
     END CATCH
 END;
 GO
+
