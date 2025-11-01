c6ae2ab837264ec4bb35a1e04ee39105b86b6b3f
mostafakhaled88 <mosta.mk@gmail.com>
2025-11-01 03:03:55 +0200

    Update KeepDigits.sql

--- scripts/KeepDigits.sql
+++ scripts/KeepDigits.sql
@@ -1,24 +1,16 @@
-USE SalesDWH;
-GO
-
-IF OBJECT_ID('dbo.KeepDigits', 'FN') IS NOT NULL
-    DROP FUNCTION dbo.KeepDigits;
-GO
-
-CREATE FUNCTION dbo.KeepDigits (@input NVARCHAR(100))
-RETURNS NVARCHAR(100)
+CREATE FUNCTION dbo.KeepDigits (@input NVARCHAR(4000))
+RETURNS NVARCHAR(4000)
 AS
 BEGIN
-    DECLARE @output NVARCHAR(100) = '';
-    DECLARE @i INT = 1;
+    DECLARE @output NVARCHAR(4000);
 
-    WHILE @i <= LEN(@input)
-    BEGIN
-        IF SUBSTRING(@input, @i, 1) LIKE '[0-9]'
-            SET @output += SUBSTRING(@input, @i, 1);
-        SET @i += 1;
-    END
+    WITH Tally AS (
+        SELECT TOP (LEN(@input)) ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS n
+        FROM sys.objects
+    )
+    SELECT @output = STRING_AGG(SUBSTRING(@input, n, 1), '')
+    FROM Tally
+    WHERE SUBSTRING(@input, n, 1) LIKE '[0-9]';
 
     RETURN @output;
 END;
-GO
